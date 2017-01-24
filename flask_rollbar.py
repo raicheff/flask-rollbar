9cb9183e965299794ec6aaef08e05d82b7a63f4b
Boris Raicheff <boris@MacBook-Pro.local>
2017-01-24 16:22:10 +0000

    Initial import

---
+++ flask_rollbar.py
@@ -0,0 +1,80 @@
+#
+# Flask-Rollbar
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+import abc
+
+import rollbar
+import rollbar.contrib.flask
+
+from flask import got_request_exception
+
+
+__version__ = '0.1.0'
+
+
+class RollbarRequestMixin(object):
+    """
+    https://github.com/rollbar/rollbar-flask-example
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractproperty
+    def rollbar_person(self):
+        return None
+
+
+class Rollbar(object):
+    """
+    Flask-Rollbar
+
+    https://rollbar.com/docs/notifier/pyrollbar/
+    https://github.com/rollbar/rollbar-flask-example
+
+    https://github.com/angstwad/flask-rollbar
+    https://github.com/psykzz/flask-rollbar
+    """
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app, **kwargs):
+
+        enabled = app.config.get('ROLLBAR_ENABLED', not app.debug)
+        if not enabled:
+            return
+
+        access_token = app.config.get('ROLLBAR_SERVER_TOKEN')
+        environment = app.config.get('ROLLBAR_ENVIRONMENT')
+
+        def _hook(request, data):
+            # https://rollbar.com/docs/api/items_post
+            # data['platform'] = 'linux'
+            data['language'] = 'python'
+            data['framework'] = 'flask'
+            if request:
+                data['context'] = str(request.url_rule)
+
+        rollbar.BASE_DATA_HOOK = _hook
+
+        rollbar.init(
+            access_token,
+            environment,
+            root=app.root_path,
+            allow_logging_basic_config=False,
+            **kwargs
+        )
+
+        got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+    def __getattr__(self, name):
+        return getattr(rollbar, name)
+
+
+# EOF
---
+++ setup.py
@@ -0,0 +1,26 @@
+#
+# Flask-Rollbar
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+from setuptools import setup
+
+from .flask_rollbar import __version__
+
+
+setup(
+    name='Flask-Rollbar',
+    version=__version__,
+    description='Flask-Rollbar',
+    author='Boris Raicheff',
+    author_email='b@raicheff.com',
+    url='https://github.com/raicheff/flask-rollbar',
+    install_requires=('flask', 'rollbar'),
+    py_modules=('flask_rollbar',),
+)
+
+
+# EOF
